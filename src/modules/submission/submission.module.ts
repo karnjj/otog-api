c81761f5ac77300bce2d5a8e3c27aba0118c023c
Karnjj <karnjj@hotmail.com>
2021-06-23 16:11:29 +0700

    🐛import jwt module to submission

--- src/modules/submission/submission.module.ts
+++ src/modules/submission/submission.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { JWT_PRIVATE } from 'src/core/constants';
 import { ContestModule } from '../contest/contest.module';
 import { SubmissionController } from './submission.controller';
+import { SubmissionGateway } from './submission.gateway';
 import { submissionProvider } from './submission.provider';
 import { SubmissionService } from './submission.service';
 
 @Module({
-  imports: [ContestModule],
+  imports: [
+    JwtModule.register({
+      secret: JWT_PRIVATE,
+      signOptions: {
+        algorithm: 'RS256',
+        expiresIn: '5h',
+      },
+    }),
+    ContestModule,
+  ],
   controllers: [SubmissionController],
-  providers: [SubmissionService, ...submissionProvider],
+  providers: [SubmissionService, SubmissionGateway, ...submissionProvider],
   exports: [SubmissionService],
 })
 export class SubmissionModule {}
